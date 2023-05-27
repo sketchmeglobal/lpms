b4e6aa0f810de48fae21c33b48c0cf50e924696a
sketchmeglobal <sketchmeglobal@gmail.com>
2023-05-27 17:47:23 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-    'password' => '',
-	'database' => 'sopl_2023',
+	'username' => 'sopl_shilpa_2022',
+    'password' => 'U.Uj^M=r0%Uo',
+    'database' => 'sopl_2023',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
